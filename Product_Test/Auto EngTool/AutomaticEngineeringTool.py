28e13bc2bcbcd7f4e2db62336954ecda277b8aba
Lucca Klassen <lucca.klassen@volvo.com>
2025-09-04 08:03:28 -0300

    Adiciona execução do EngineeringTool e espera de 20 segundos

--- Product_Test/Auto EngTool/AutomaticEngineeringTool.py
+++ Product_Test/Auto EngTool/AutomaticEngineeringTool.py
@@ -1,1 +1,12 @@
 import pandas as pd
+import os
+import time
+
+user_windows = os.getlogin()
+
+# Caminho do executável ou atalho
+eng_tool = r"C:\Program Files (x86)\AB Volvo\EngineeringTool_2.0\EngineeringTool.DesktopClient.Shell.exe"
+os.startfile(eng_tool)
+
+# Aguarda 20 segundos
+time.sleep(20)
