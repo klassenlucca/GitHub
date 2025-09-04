395e90a2bdd412653342f548f96b6eeda8ba3b93
Lucca Klassen <lucca.klassen@volvo.com>
2025-09-04 08:09:21 -0300

    Troca a execução do Engineering Tool para a execução do Console para inserir comandos

--- Product_Test/Auto EngTool/AutomaticEngineeringTool.py
+++ Product_Test/Auto EngTool/AutomaticEngineeringTool.py
@@ -5,7 +5,7 @@ import time
 user_windows = os.getlogin()
 
 # Caminho do executável ou atalho
-eng_tool = r"C:\Program Files (x86)\AB Volvo\EngineeringTool_2.0\EngineeringTool.DesktopClient.Shell.exe"
+eng_tool = r"C:\Program Files (x86)\AB Volvo\EngineeringTool_2.0\ConsoleEngineeringTool2.0.exe"
 os.startfile(eng_tool)
 
 # Aguarda 20 segundos
